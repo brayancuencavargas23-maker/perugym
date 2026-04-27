741c9a5e00561002d060d6c647796b77fe7a0f10
Brayan Vargas <brayancuencavargas23@gmail.com>
2026-04-27 17:54:36 -0500

    update: cambios en utils.js

--- public/js/utils.js
+++ public/js/utils.js
@@ -109,6 +109,7 @@ function showCardsSkeleton(containerId, count = 6) {
 }
 
 
+function openModal(id) { document.getElementById(id).classList.remove('hidden'); }
 function closeModal(id) { document.getElementById(id).classList.add('hidden'); }
 
 // Format date
